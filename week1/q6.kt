d033effe5e6029670bf2e95791c8af018cdad726
akabhijith <75904795+akabhijith@users.noreply.github.com>
2020-12-18 16:29:27 +0530

    Create q6.kt

---
+++ week1/q6.kt
@@ -0,0 +1,9 @@
+fun main() {
+   val c= 'j'
+    if (c in 'a'..'z')
+    print("Yes it is an alphabet")
+    else if(c in 'A'..'Z')
+    print("Yes it is an alphabet")
+    else
+    print("No it is not an aphabet")
+}
